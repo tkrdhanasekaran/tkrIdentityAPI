5298c3887a5e5201482b68b7957045dd52e46e21
Hvuser <spriyaraj22@gmail.com>
2019-11-08 11:45:44 +0530

    first commit

---
+++ IdentityAPI/Helpers/StorageAccountHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityAPI.Helpers
+{
+    public class StorageAccountHelper
+    {
+        private string storageConnectionString;
+        private CloudStorageAccount storageAccount;
+        private CloudQueueClient queueClient;
+
+        public string StorageConnectionString
+        {
+            get { return storageConnectionString; }
+            set
+            {
+                this.storageConnectionString = value;
+                storageAccount = CloudStorageAccount.Parse(this.storageConnectionString);
+            }
+        }
+
+        public async Task SendMessageAsync(string message, string queueName)
+        {
+            queueClient = storageAccount.CreateCloudQueueClient();
+            var queue = queueClient.GetQueueReference(queueName);
+            await queue.CreateIfNotExistsAsync();
+            CloudQueueMessage queueMessage = new CloudQueueMessage(message);
+            await queue.AddMessageAsync(queueMessage);
+
+        }
+    }
+}
---
+++ IdentityAPI/Startup.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using IdentityAPI.Infrastructure;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace IdentityAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<IdentityDbContext>(config =>
+            {
+                config.UseSqlServer(Configuration.GetConnectionString("IdentityConnection"));
+                // config.UseInMemoryDatabase("IdentityDb");
+            });
+
+            services.AddCors(c =>
+            {
+                c.AddDefaultPolicy(x => x.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+                c.AddPolicy("AllowPartners", x =>
+                {
+                    x.WithOrigins("http://microsoft.com", "https://synergetics.com")
+                    .WithMethods("GET", "POST")
+                    .AllowAnyHeader();
+                });
+
+                c.AddPolicy("AllowAll", x =>
+                {
+                    x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info
+                {
+                    Title = "Identity API",
+                    Description = "Authentication methods for Eshop application",
+                    Version = "1.0",
+                });
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwaggerUI(config =>
+                {
+                    config.SwaggerEndpoint("/swagger/v1/swagger.json", "Identity API");
+                    config.RoutePrefix = "";
+                });
+
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseCors("AllowAll");
+            InitialiseDatabase(app);
+            app.UseMvc();
+        }
+
+        private void InitialiseDatabase(IApplicationBuilder app)
+        {
+            //it uses the migration classes to create the table
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                using (var dbContext = serviceScope.ServiceProvider.GetRequiredService<IdentityDbContext>())
+                {
+                    dbContext.Database.Migrate();
+                }
+            }
+        }
+    }
+}
